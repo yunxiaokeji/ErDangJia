c873392818bf10564bfb1cfc5ae551279d8d506e
w609365252 <609365252@qq.com>
2016-07-08 16:18:33 +0800

    7.5调整

--- YXApp/Controllers/HomeController.cs
+++ YXApp/Controllers/HomeController.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using YunXiaoService;
+using IntFactory.Sdk;
 namespace YXApp.Controllers
 {
     public class HomeController : Controller
     {
         //
         // GET: /Home/
+        Dictionary<string, object> JsonDictionary = new Dictionary<string, object>();
 
+        #region view
         public ActionResult Login()
         {
             return View();
@@ -24,7 +28,21 @@ namespace YXApp.Controllers
         public ActionResult Index()
         {
             return View();
+        } 
+        #endregion
+
+        #region ajax
+        public JsonResult GetCustomerBaseInfo(string customerID, string clientID) 
+        {
+            var item = CustomerBusiness.BaseBusiness.GetCustomerByID(customerID, clientID);
+            JsonDictionary.Add("item", item);
+            return new JsonResult
+            {
+                Data = JsonDictionary,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
+        #endregion
 
     }
 }
---
+++ YXApp/modules/scripts/home/register.js
@@ -0,0 +1,35 @@
+﻿
+
+define(function (require, exports, module) {
+
+    require("jquery");
+
+    var Global = require("global")
+
+    var ObjectJS = {};
+
+    ObjectJS.init = function (customerID, clientID) {
+        ObjectJS.customerID = customerID;
+        ObjectJS.clientID = clientID;
+        ObjectJS.getCustomerBaseInfo();
+        ObjectJS.bindEvent();
+    };
+
+    ObjectJS.bindEvent = function () {
+        $("#btnRegister").click(function () {
+            if (!$("#loginName").val()) {
+                $(".registerErr").html("请输入账号").slideDown();
+                return;
+            }
+        });
+    };
+
+    //判断用户是否存在
+    ObjectJS.getCustomerBaseInfo = function () {
+        Global.post("/Home/GetCustomerBaseInfo", { customerID: ObjectJS.customerID, clientID: ObjectJS.clientID }, function (data) {
+            console.log(data);
+        });
+    };
+
+    module.exports = ObjectJS;
+});

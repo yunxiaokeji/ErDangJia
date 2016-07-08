290f2f12283f55c49a073779c6fb8306f32a5e65
w609365252 <609365252@qq.com>
2016-07-08 14:23:21 +0800

    7.5调整

---
+++ YXApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YXApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult ProductsLists()
+        {
+            return View();
+        }
+
+    }
+}

8a38dc55bc577f3f7b528c0a7dc06ed79ce52b25
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-07-11 16:29:08 +0800

    调整

---
+++ IntFactory.Sdk/Business/OrderBusiness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntFactory.Sdk.Business
+{
+    public class OrderBusiness
+    {
+        public static OrderBusiness BaseBusiness = new OrderBusiness();
+
+        public OrderListResult GetOrdersByYXClientCode(string yxClientCode, string zngcClientID = "")
+        {
+            var paras = new Dictionary<string, object>();
+            paras.Add("yxClientCode", yxClientCode);
+            paras.Add("zngcClientID", zngcClientID);
+
+            return HttpRequest.RequestServer<OrderListResult>(ApiOption.GetOrdersByYXClientCode, paras);
+        }
+    }
+}
---
+++ IntFactory.Sdk/Entity/Client/ClientEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntFactory.Sdk
+{
+    public class ClientEntity
+    {
+        public string clientID;
+
+        public string companyName;
+
+        public string logo;
+
+        public string contactName;
+
+        public string mobilePhone;
+
+        public string address;
+
+    }
+}

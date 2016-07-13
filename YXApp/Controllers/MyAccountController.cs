95d8f348fe423bb66a3aece2edcaeea4da3c99b0
w609365252 <609365252@qq.com>
2016-07-13 16:55:27 +0800

    7.12调整

--- YXApp/Controllers/MyAccountController.cs
+++ YXApp/Controllers/MyAccountController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Script.Serialization;
 namespace YXApp.Controllers
 {
     public class MyAccountController : BaseController
@@ -31,6 +31,21 @@ namespace YXApp.Controllers
             OrderResult item = OrderBusiness.BaseBusiness.GetOrderDetailByID(orderID, zngcClientID);
             ViewBag.DetailItem = item;
             return View();
+        }
+
+        //创建订单
+        public JsonResult CreateOrder(string zngcOrderID,decimal price,string detailsEntity,string zngcClientID)
+        {
+            JavaScriptSerializer serializer=new JavaScriptSerializer();
+            List<ProductDetailEntity> details = serializer.Deserialize<List<ProductDetailEntity>>(detailsEntity);
+            string yxOrderID="";
+            AddResult result = OrderBusiness.BaseBusiness.CreateDHOrder(zngcOrderID, price, details, zngcClientID, yxOrderID);
+            JsonDictionary.Add("result", result);
+            return new JsonResult
+            {
+                Data = JsonDictionary,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
     }
 }

e85c992fd708d754fe9a5141872488a919c3ca6c
w609365252 <609365252@qq.com>
2016-07-13 14:34:52 +0800

    7.12调整

--- YXApp/modules/scripts/myaccount/historyorderdetail.js
+++ YXApp/modules/scripts/myaccount/historyorderdetail.js
@@ -3,13 +3,13 @@
     var Global = require("global"),
         doT = require("dot");
     var ObjectJS = {};
+    var CacheArr = new Array();
 
     ObjectJS.init = function () {
         ObjectJS.bindEvent();
     };
 
     ObjectJS.bindEvent = function () {
-
         $(".add-shopping").click(function () {
             //doT.exec("template/product/shopping-list.html",function (template) {
             //    var innerHtml = template();
@@ -18,12 +18,42 @@
             //});
         });
 
+        $(".color-item").click(function () {
+            var _this = $(this);
+            if (!_this.hasClass('hover')) {
+                $('.txt-model').val(0);
+                $(".color-item").removeClass('hover');
+                _this.addClass('hover');
+                console.log(CacheArr);
+            }
+        });
+
         $(".menu-module").click(function () {
             var _this = $(this);
             if (!_this.hasClass('hover')) {
                 $(".menu-module").removeClass('hover');
                 _this.addClass('hover');
                 $('.content-main div[data-module=' + _this.attr('id') + ']').show().siblings().hide();
+            }
+        });
+
+        $(".txt-model").blur(function () {
+            var _this = $(this);
+            if ($(".color-item.hover").data(_this.data('name')) == _this.data('id')) {
+                CacheArr[$(".color-item.hover").data('id')][$(".color-item.hover").data('id') + _this.data('id')].Quantity = _this.val();
+                alert(CacheArr[$(".color-item.hover").data('id')][$(".color-item.hover").data('id') + _this.data('id')].Quantity);
+            } else {
+                var model = [];
+                var modelDetail = {
+                    Quantity: $(_this).val(),
+                    SaleAttr: $(".color-box").data('id') + ',' + $(".attr-box").data('id'),
+                    AttrValue: $(".color-item.hover").data('id') + ',' + _this.data('id'),
+                    SaleAttrValue: $(".color-box").data('id') + ":" + $(".color-item.hover").data('id') + "," + $(".attr-box").data('id') + _this.data('id'),
+                    Description: '[颜色:' + $(".color-item.hover").data('value') + ',尺码:' + _this.data('value') + ']'
+                };
+                model[$(".color-item.hover").data('id') + _this.data('id')] = modelDetail;
+                CacheArr[$(".color-item.hover").data('id')] = model;
+                $(".color-item.hover").data(_this.data('name'), _this.data('id'));
             }
         });
     };

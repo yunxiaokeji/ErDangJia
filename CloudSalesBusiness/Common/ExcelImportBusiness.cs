b2e076c3d4bf4b5a4bd624c840b242d096d6f150
sunallen521 <sunallen@163.com>
2016-07-18 10:23:29 +0800

    供应商

--- CloudSalesBusiness/Common/ExcelImportBusiness.cs
+++ CloudSalesBusiness/Common/ExcelImportBusiness.cs
@@ -62,7 +62,7 @@ namespace CloudSalesBusiness
             {
                 int result = 0;
                 string pid
-                    = ProductsBusiness.BaseBusiness.AddProduct(x.ProductCode, x.ProductName, x.GeneralName, (x.IsCombineProduct == 1), x.BrandID,
+                    = ProductsBusiness.BaseBusiness.AddProduct(x.ProductCode, x.ProductName, x.GeneralName, (x.IsCombineProduct == 1), x.ProviderID, x.BrandID,
                     x.BigUnitID, x.UnitID, x.BigSmallMultiple.Value, x.CategoryID, x.Status.Value, x.AttrList, x.ValueList, x.AttrValueList,
                     x.CommonPrice.Value, x.Price, x.Weight.Value, (x.IsNew == 1), (x.IsRecommend == 1), x.IsAllow, x.IsAutoSend, x.EffectiveDays.Value,
                     x.DiscountValue.Value, x.WarnCount, x.ProductImage, x.ShapeCode, x.Description, x.ProductDetails, x.CreateUserID, agentid, x.ClientID, out result);

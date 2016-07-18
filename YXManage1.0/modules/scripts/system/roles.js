f71bd068c7adac10e2258fd86ea3574966f6708f
Michauxquan <79365450@qq.com>
2016-07-18 10:45:07 +0800

    [F]  temp

--- YXManage1.0/modules/scripts/system/roles.js
+++ YXManage1.0/modules/scripts/system/roles.js
@@ -244,7 +244,13 @@
                         $("#" + _this.attr("data-id")).hide();
                     }
                 });
+                if (ObjectJS.getClass(_this.data("id")) != "") {
+                    $('label[data-cid="' + _this.data("id") + '"]').removeClass("check").removeClass("checked").removeClass("checknotall").addClass(ObjectJS.getClass(_this.data("id")));
+                    _self.checkRefresh(_this.data("id"));
+
+                }
             });
+
         });
     }
 
@@ -264,13 +270,14 @@
                 _leftBg.append("<span class='line left'></span>");
             }
             _item.append(_leftBg);
-
+            var haschildren = false;
             //是否最后一位
             if (i == cacheMenu[menuCode].length - 1) {
                 _item.append("<span class='lastline left'></span>");
 
                 //加载显示下属图标和缓存数据
                 if (cacheMenu[menuCode][i].ChildMenus && cacheMenu[menuCode][i].ChildMenus.length > 0) {
+                    haschildren = true;
                     _item.append("<span data-id='" + cacheMenu[menuCode][i].MenuCode + "' data-eq='last' data-state='close' class='icoopen openchild left'></span>");
                     if (!cacheMenu[cacheMenu[menuCode][i].MenuCode]) {
                         cacheMenu[cacheMenu[menuCode][i].MenuCode] = cacheMenu[menuCode][i].ChildMenus;
@@ -281,6 +288,7 @@
 
                 //加载显示下属图标和缓存数据
                 if (cacheMenu[menuCode][i].ChildMenus && cacheMenu[menuCode][i].ChildMenus.length > 0) {
+                    haschildren = true;
                     _item.append("<span data-id='" + cacheMenu[menuCode][i].MenuCode + "' data-eq='' data-state='close' class='icoopen openchild left'></span>");
                     if (!cacheMenu[cacheMenu[menuCode][i].MenuCode]) {
                         cacheMenu[cacheMenu[menuCode][i].MenuCode] = cacheMenu[menuCode][i].ChildMenus;
@@ -288,32 +296,40 @@
                 }
             }
 
-            _item.append("<label class='check left'><input type='checkbox' class='left'  value='" + cacheMenu[menuCode][i].MenuCode + "' data-id='" + cacheMenu[menuCode][i].MenuCode + "' /><span>" + cacheMenu[menuCode][i].Name + "</span></label>");
+            _item.append("<label class='check left' data-pcode='" + cacheMenu[menuCode][i].PCode + "' data-cid='"+cacheMenu[menuCode][i].MenuCode+"'><input type='checkbox' class='left'  value='" + cacheMenu[menuCode][i].MenuCode + "'  data-id='" + cacheMenu[menuCode][i].MenuCode + "' /><span>" + cacheMenu[menuCode][i].Name + "</span></label>");
 
             _div.append(_item);
 
             _item.find("input").change(function () {
                 var _this = $(this);
-                if (_this.prop("checked")) {
-                    _this.parent().addClass("checked").removeClass("check");
+                console.log(_this.prop("checked"));
+                if (_this.prop("checked") &&( _this.parent().hasClass("check") || _this.parent().hasClass("checknotall"))) {
+                    
                     $("#" + _this.data("id")).find("input").prop("checked", _this.prop("checked"));
-                    $("#" + _this.data("id")).find("label").addClass("checked").removeClass("check");
+                    $("#" + _this.data("id")).find("label").addClass("checked").removeClass("check").removeClass("checknotall");
                     _this.parents().each(function () {
                         var _parent = $(this);
                         if (_parent.hasClass("childbox")) {
                             _parent.prev().find("input").prop("checked", true);
                             _parent.prev().find("label").addClass("checked").removeClass("check");
                         }
-                    });
-                } else {
-                    _this.parent().addClass("check").removeClass("checked");
+                    }); 
+                    _this.parent().addClass("checked").removeClass("check").removeClass("checknotall");
+                    _self.checkRefresh(_this.data("id"));
+                }  else {
+                    _this.parent().addClass("check").removeClass("checked").removeClass("checknotall");
                     $("#" + _this.data("id")).find("input").prop("checked", _this.prop("checked"));
-                    $("#" + _this.data("id")).find("label").addClass("check").removeClass("checked");
+                    $("#" + _this.data("id")).find("label").addClass("check").removeClass("checked").removeClass("checknotall");
+
+                    _self.checkRefresh(_this.data("id"));
+                    //if(ObjectJS.getClass(_this.data("id"))!=""){
+                    //    $('label[data-cid="' + _this.data("id") + '"]').removeClass("check").removeClass("checked").removeClass("checknotall").addClass(ObjectJS.getClass(_this.data("id")));
+                    //}
                 }
             });
             //默认加载下级
             _item.find(".openchild").each(function () {
-                var _this = $(this);
+                var _this = $(this); 
                 var _obj = _self.getChild(_this.attr("data-id"), _leftBg.html(), _this.attr("data-eq"), menus);
                 _this.parent().after(_obj);
                 _this.on("click", function () {
@@ -329,7 +345,7 @@
 
                         $("#" + _this.attr("data-id")).hide();
                     }
-                });
+                });               
             });
         }
 
@@ -342,9 +358,50 @@
                     _this.parent().addClass("checked").removeClass("check");
                 }
             }
+        }); 
+        _div.find(".check").each(function (i, v) {
+            var _this = $(v);
+            var templist = _div.find("[data-cid = '" + _this.attr("data-pcode") + "']"); 
+            if (templist.length > 0) {
+                $(_div.find("[data-cid = '" + _this.attr("data-pcode") + "']")[0]).removeClass("check").removeClass("check").addClass("checknotall");
+            }
+        }); 
+        _div.find(".checknotall").each(function (i,v) {
+            var _this = $(v);
+            var templist = _div.find("[data-cid = '" + _this.attr("data-pcode") + "']");
+            if (templist.length > 0) {
+                $(_div.find("[data-cid = '" + _this.attr("data-pcode") + "']")[0]).removeClass("checked").addClass("checknotall");
+            }
         });
         return _div;
     }
-
+    ObjectJS.checkRefresh = function (pcode) {
+        if ($("label[data-pcode='" + pcode + "']").length > 0) {
+            $("label[data-cid='" + pcode + "']").each(function(i, v) {
+                var classname = ObjectJS.getClass($(v).data("cid"));
+                console.log(classname);
+                if (classname != "") {
+                    $(v).removeClass("check").removeClass("checked").removeClass("checknotall").addClass(classname);
+                   //ObjectJS.checkchildRefresh($(v).data("cid"));
+                }
+            });
+        } else {
+            return false;
+        }
+    } 
+     
+    ObjectJS.getClass = function (divid) {
+        console.log($("label[data-pcode='" + divid + "']").hasClass("check") );
+        if ($("label[data-pcode='" + divid + "']").length == 0) {
+            return "";
+        }
+        if ($("label[data-pcode='" + divid + "']").hasClass("check") && !$("label[data-pcode='" + divid + "']").hasClass("checknotall")) {
+            return "check";
+        } else if ($("label[data-pcode='" + divid + "']").hasClass("check") || $("label[data-pcode='" + divid + "']").hasClass("checknotall")) {
+            return "checknotall";
+        } else {
+            return "checked";
+        }
+    }
     module.exports = ObjectJS;
 });

0492125aac5bbf915a8be8013fe9d78a919abc87
Michaux <79365450@qq.com>
2016-08-01 18:53:59 +0800

    [F] base OpenApi
    [F] 错误页面移植

---
+++ YXERP/Areas/Api/Controllers/BaseAPIController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using  YXERP.Common;
+namespace YXERP.Areas.Api.Controllers
+{
+    public class BaseAPIController : Controller
+    {
+        /// <summary>
+        /// 返回数据集合
+        /// </summary>
+        protected Dictionary<string, object> JsonDictionary = new Dictionary<string, object>()
+        {
+          {"error_code",0}, {"error_msg",""}
+        }; 
+        protected int PageSize = 10;
+
+        public static string ApiUrl = Common.Common.GetXmlNodeValue("root/ZNGCApi", "ApiUrl") ?? "http://localhost:8888";
+        public static string AppKey = Common.Common.GetXmlNodeValue("root/ZNGCApi", "AppKey");
+        public static string AppSecret = Common.Common.GetXmlNodeValue("root/ZNGCApi", "AppSecret");
+        public static string CallBackUrl = Common.Common.GetXmlNodeValue("root/ZNGCApi", "CallBackUrl");
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            try
+            {
+                this.View(actionName).ExecuteResult(this.ControllerContext);
+            }
+            catch (InvalidOperationException ieox)
+            {
+                ViewData["error"] = "Unknown Action: \"" + Server.HtmlEncode(actionName) + "\"";
+                ViewData["exMessage"] = ieox.Message;
+                ViewData["urlReferrer"] = Request != null && Request.Url!=null ? Request.Url.ToString() : "";
+                this.View("Error").ExecuteResult(this.ControllerContext);
+            }
+        }
+    }
+}
---
+++ YXERP/Areas/Api/Controllers/PurchaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YXERP.Areas.Api.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        //
+        // GET: /Api/Purchase/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

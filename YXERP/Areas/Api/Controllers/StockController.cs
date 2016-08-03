d03ad1f5600fcaff17a4ca179b03cc23606e93ba
Michaux <79365450@qq.com>
2016-08-03 10:27:24 +0800

    [F] 1

---
+++ YXERP/Areas/Api/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YXERP.Areas.Api.Controllers
+{
+    [YXERP.Common.ApiAuthorize]
+    public class StockController : BaseAPIController
+    {
+        //
+        // GET: /Api/Purchase/
+
+        public ActionResult GetPurchases(string userid, int status, string keywords, string begintime, string endtime, string wareid, string providerid, int pageSize, int pageIndex, ref int totalCount, ref int pageCount, string agentid, string clientid)
+        {
+           
+             
+        }
+
+    }
+}
--- YunXiaoService/StockService.cs
+++ YunXiaoService/StockService.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using CloudSalesEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CloudSalesBusiness;
+using CloudSalesEnum;
 
 namespace YunXiaoService
 {
     public class StockService
     {
+        public static List<StorageDoc> GetPurchases(string userid, int status, string keywords, string begintime, string endtime, string wareid, string providerid, int pageSize, int pageIndex, ref int totalCount, ref int pageCount, string agentid, string clientid)
+        {
+           EnumDocStatus statustype= Enum.Parse(typeof (EnumDocStatus), status.ToString());
+
+            return StockBusiness.GetPurchases(userid, Enum.Parse(typeof(EnumDocStatus), status.ToString()), keywords, begintime, endtime, wareid, providerid, pageSize,
+                pageIndex, ref totalCount, ref pageCount, agentid, clientid);
+        }
 
     }
 }

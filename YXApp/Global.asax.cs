3ef56382f6bf9828bccfb40198d679195e5d385a
sunallen521 <sunallen@163.com>
2016-08-14 15:58:29 +0800

    删除

--- YXApp/Global.asax.cs
+++ YXApp/Global.asax.cs
@@ -15,8 +15,6 @@ namespace YXApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
